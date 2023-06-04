90812a4cbcad6a668888d1df42496fb9b240aab1
Walttinho <walter.netto@live.com>
2023-06-04 01:03:09 -0300

    ajust username of database

--- src/database/db.js
+++ src/database/db.js
@@ -4,7 +4,7 @@ const connectDatabase = () => {
   console.log("Wait connecting to the database");
   mongoose
     .connect(
-      "mongodb+srv://Walttinho:W@ltinho0o@cluster0.q5hhxfs.mongodb.net/?retryWrites=true&w=majority",
+      "mongodb+srv://Walttinho:Waltinho0o925837@cluster0.wrmswnn.mongodb.net/?retryWrites=true&w=majority",
       { useNewUrlParser: true, useUnifiedTopology: true }
     )
     .then(() => console.log("MongoDB Atlas Connected"))
