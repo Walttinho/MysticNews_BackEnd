c994c0ce9af8ccc4feaefe5e51ec3fd0be661aff
Walttinho <walter.netto@live.com>
2023-06-15 17:09:37 -0300

    feat middleware

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,27 +1,12 @@
-<<<<<<< HEAD
 import { config, jwt } from "../../config.js";
-=======
-import dotenv from "dotenv";
-import jwt from "jsonwebtoken";
-import { config } from "../config/config.js"
->>>>>>> c4d61ee70c04b90e5991deea58aa75604acf76e1
 import userService from "../services/user.service.js";
 
 // Middleware de autenticação
-<<<<<<< HEAD
-=======
-// Authentication middleware
->>>>>>> c4d61ee70c04b90e5991deea58aa75604acf76e1
 export const authMiddleware = (req, res, next) => {
   try {
     const { authorization } = req.headers;
 
-<<<<<<< HEAD
 // Verifica se o cabeçalho de autorização foi fornecido
-=======
-    // Verifica se o cabeçalho de autorização foi fornecido
-    // Check if the authorization header is provided
->>>>>>> c4d61ee70c04b90e5991deea58aa75604acf76e1
     if (!authorization) {
       return res
         .status(401)
@@ -29,49 +14,29 @@ export const authMiddleware = (req, res, next) => {
     }
 
     const parts = authorization.split(" ");
-<<<<<<< HEAD
 
      // Verifica se o cabeçalho de autorização está no formato correto
-=======
-    // Verifica se o cabeçalho de autorização está no formato correto
-    // Check if the authorization header is in the correct format
->>>>>>> c4d61ee70c04b90e5991deea58aa75604acf76e1
     if (parts.length !== 2) {
       return res.status(401).send({ message: "Invalid authorization header" });
     }
 
     const [schema, token] = parts;
-<<<<<<< HEAD
 
     // Verifica se o esquema de autorização é válido
-=======
-    // Verifica se o esquema de autorização é válido
-    // Check if the authorization schema is valid
->>>>>>> c4d61ee70c04b90e5991deea58aa75604acf76e1
     if (schema !== "Bearer") {
       return res.status(401).send({ message: "Invalid authorization schema" });
     }
 
     jwt.verify(token, config.jwtSecret, async (error, decoded) => {
-<<<<<<< HEAD
 
       // Verifica se o token é válido
-=======
-      // Verifica se o token é válido
-      // Check if the token is valid
->>>>>>> c4d61ee70c04b90e5991deea58aa75604acf76e1
       if (error) {
         return res.status(401).send({ message: "Invalid token" });
       }
 
       const user = await userService.findByIdService(decoded.id);
-<<<<<<< HEAD
 
     // Verifica se o usuário associado ao token existe  
-=======
-      // Verifica se o usuário associado ao token existe
-      // Check if the user associated with the token exists
->>>>>>> c4d61ee70c04b90e5991deea58aa75604acf76e1
       if (!user || !user.id) {
         return res.status(401).send({ message: "Invalid token" });
       }
@@ -81,10 +46,6 @@ export const authMiddleware = (req, res, next) => {
     });
   } catch (error) {
     // Captura e trata qualquer erro
-<<<<<<< HEAD
-=======
-    // Catch and handle any errors
->>>>>>> c4d61ee70c04b90e5991deea58aa75604acf76e1
     console.error("Authentication error:", error);
     res.status(500).send({ message: "Authentication error" });
   }
--- src/middlewares/global.middleware.js
+++ src/middlewares/global.middleware.js
@@ -1,28 +1,23 @@
 import mongoose from "mongoose";
-import userService from "../services/user.service.js";
+import userService  from "../services/user.service.js";
 
 // Middleware para validar o ID
-// Middleware to validate the ID
 export const validId = (req, res, next) => {
   const id = req.params.id;
 
   try {
     // Verifica se o ID fornecido é um ObjectId válido
-    // Checks if the provided ID is a valid ObjectId
     if (!mongoose.Types.ObjectId.isValid(id)) {
       return res.status(400).send({ message: "Invalid ID" });
     }
     next(); // Chama o próximo middleware ou rota
-    // Calls the next middleware or route
   } catch (error) {
     // Em caso de erro, envia uma resposta com o status 500 (Internal Server Error)
-    // In case of error, sends a response with status 500 (Internal Server Error)
     res.status(500).send({ message: "An error occurred" });
   }
 };
 
 // Middleware para validar o usuário
-// Middleware to validate the user
 export const validUser = async (req, res, next) => {
   const id = req.params.id;
 
@@ -36,12 +31,9 @@ export const validUser = async (req, res, next) => {
     req.id = id;
     req.user = user;
 
-    next();
-    // Chama o próximo middleware ou rota
-    // Calls the next middleware or route
+    next(); // Chama o próximo middleware ou rota
   } catch (error) {
     // Em caso de erro, envia uma resposta com o status 500 (Internal Server Error)
-    // In case of error, sends a response with status 500 (Internal Server Error)
     res.status(500).send({ message: "An error occurred" });
   }
 };
