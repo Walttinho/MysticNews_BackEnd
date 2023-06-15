3b6618344bf2d89a2e580dffa4dc3231348770d0
Walttinho <walter.netto@live.com>
2023-06-15 17:15:20 -0300

    feat user

--- src/models/User.js
+++ src/models/User.js
@@ -2,57 +2,44 @@ import mongoose from "mongoose";
 import { config, bcrypt } from "../../config.js";
 
 // Esquema para a coleção de usuários
-<<<<<<< HEAD
-=======
-// Schema for the users collection
->>>>>>> c4d61ee70c04b90e5991deea58aa75604acf76e1
 const UserSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true,
+    require: true,
   },
   username: {
     type: String,
-    required: true,
+    require: true,
     unique: true,
     lowercase: true,
   },
   email: {
     type: String,
-    required: true,
+    require: true,
     unique: true,
   },
   password: {
     type: String,
-    required: true,
+    require: true,
     select: false,
   },
   avatar: {
     type: String,
-    required: true,
+    require: true,
   },
   background: {
     type: String,
-    required: true,
+    require: true,
   },
 });
 
 // Função executada antes de salvar um usuário no banco de dados
-<<<<<<< HEAD
-=======
-// Function executed before saving a user to the database
->>>>>>> c4d61ee70c04b90e5991deea58aa75604acf76e1
 UserSchema.pre("save", async function (next) {
   this.password = await bcrypt.hash(this.password, config.bcryptSaltRounds);
   next();
 });
 
 // Modelo para a coleção de usuários
-<<<<<<< HEAD
-=======
-// Model for the users collection
->>>>>>> c4d61ee70c04b90e5991deea58aa75604acf76e1
 const User = mongoose.model("User", UserSchema);
 
 export default User;
-
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,26 +1,19 @@
-import { Router } from "express";
+import {Router} from "express";
 import userController from "../controllers/user.controller.js";
 import { validId, validUser } from "../middlewares/global.middleware.js";
 
 const router = Router();
 
 // Rota para criar um novo usuário
-<<<<<<< HEAD
-=======
-// Route to create a new user
->>>>>>> c4d61ee70c04b90e5991deea58aa75604acf76e1
 router.post("/create", userController.create);
 
 // Rota para obter todos os usuários
-// Route to get all users
 router.get("/", userController.findAll);
 
 // Rota para obter um usuário pelo ID
-// Route to get a user by ID
 router.get("/:id", validId, validUser, userController.findById);
 
-// Rota para atualizar um usuário pelo ID
-// Route to update a user by ID
+// Rota para obter um usuário pelo ID
 router.patch("/:id", validId, validUser, userController.updateUser);
 
 export default router;
