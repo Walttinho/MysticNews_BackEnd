88d9176732a19b9d09190d2aa5970b7dda0a0a61
Walttinho <walter.netto@live.com>
2023-06-16 01:59:14 -0300

    feat swagger

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import { config } from "./config.js";
 import userRoute from "./src/routes/user.route.js";
 import authRoute from "./src/routes/auth.route.js";
 import newsRoute from "./src/routes/news.route.js";
+import swaggerRoute from "./src/routes/swagger.route.js";
 
 const port = config.port;
 const app = express();
@@ -25,6 +26,7 @@ app.use(express.json());
 app.use("/user", userRoute);
 app.use("/auth", authRoute);
 app.use("/news", newsRoute);
+app.use("/doc", swaggerRoute);
 
 // Inicia o servidor
 app.listen(config.port, () =>
---
+++ src/routes/swagger.route.js
@@ -0,0 +1,11 @@
+import { Router } from "express";
+import swaggerUi from 'swagger-ui-express'
+import swaggerDocument from '../../swagger.json'
+
+const router = Router();
+
+
+router.use("/", swaggerUi.serve);
+router.get("/", swaggerUi.setup(swaggerDocument));
+
+export default router;
