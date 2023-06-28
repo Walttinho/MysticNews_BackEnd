dd374693f63fb284ebf11d8188cda9576ed1984e
Walttinho <walter.netto@live.com>
2023-06-28 03:47:11 -0300

    refact: clean architeture, user

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,103 +2,54 @@ import userService from "../services/user.service.js";
 
 // Função para criar um novo usuário
 const create = async (req, res) => {
-  const { name, username, email, password, avatar, background } = req.body;
-
-  // Verifica se todos os campos obrigatórios foram fornecidos
-  if (!name || !username || !email || !password || !avatar || !background) {
-    return res
-      .status(400)
-      .send({ message: "Submit all fields for registration" });
-  }
+  const body = req.body;
 
   try {
-    // Cria o usuário usando o serviço de criação do usuário
-    const user = await userService.createService(req.body);
+    const user = await userService.createService(body);
 
-    if (!user) {
-      return res.status(400).send({ message: "Error Creating User" });
-    }
-
-    // Retorna a resposta com o usuário criado
-    res.status(201).send({
-      message: "User created Successfully",
-      user: {
-        id: user._id,
-        name,
-        username,
-        email,
-        avatar,
-        background,
-      },
-    });
-  } catch (error) {
-    // Captura e trata qualquer erro
-    console.error("Error creating user:", error);
-    res.status(500).send({ message: "Error creating user" });
+    return res.status(201).send(user);
+  } catch (e) {
+    res.status(500).send(e.message);
   }
 };
 
 // Função para obter todos os usuários
 const findAll = async (req, res) => {
   try {
-    // Obtém todos os usuários usando o serviço de busca de todos os usuários
     const users = await userService.findAllService();
-
-    if (users.length === 0) {
-      return res.status(400).send({ message: "There are no registered users" });
-    }
-
-    // Retorna a resposta com todos os usuários
-    res.send(users);
-  } catch (error) {
-    // Captura e trata qualquer erro
-    console.error("Error finding users:", error);
-    res.status(500).send({ message: "Error finding users" });
+    return res.send(users);
+  } catch (e) {
+    res.status(500).send(e.message);
   }
 };
 
 // Função para obter um usuário pelo ID
 const findById = async (req, res) => {
+  const { id: userId } = req.params;
+  const userIdLogged = req.userId;
+
   try {
-    const user = req.user;
-    res.send(user);
-  } catch (error) {
-    // Captura e trata qualquer erro
-    console.error("Error finding user:", error);
-    res.status(500).send({ message: "Error finding user" });
+    const user = await userRepository.findByIdRepository(userId, userIdLogged);
+
+    return res.send(user);
+  } catch (e) {
+    res.status(500).send(e.message);
   }
 };
 
 // Função para atualizar um usuário
 const updateUser = async (req, res) => {
-  const { name, username, email, password, avatar, background } = req.body;
-
-  // Verifica se algum campo foi preenchido
-  if (!name && !username && !email && !password && !avatar && !background) {
-    return res
-      .status(400)
-      .send({ message: "Submit at least one field for update" });
-  }
-
-  const { id, user } = req;
+ const body = req.body
+ const userId = req.userId
 
   try {
-    await userService.updateService(
-      id,
-      name,
-      username,
-      email,
-      password,
-      avatar,
-      background
-    );
+    const response = await userService.updateService(body, userId);
 
     // Retorna a resposta de sucesso
-    res.send({ message: "User successfully updated" });
-  } catch (error) {
-    // Captura e trata qualquer erro
-    console.error("Error updating user:", error);
-    res.status(500).send({ message: "Error updating user" });
+    return res.send(response);
+  } catch (e) {
+   
+    res.status(500).send(e.message);
   }
 };
 
--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,32 +1,91 @@
-import User from "../models/User.js";
+import { bcrypt } from "../../config.js";
+import userRepository from "../Repository/user.Repository.js";
+import authService from "../services/auth.service.js";
+import { bcryptSaltRounds, bcrypt } from "../../config.js";
 
-// Serviço para criar um novo usuário
-const createService = (body) => User.create(body);
+// Função para criar um novo usuário
+const createService = async (body) => {
+  const { name, username, email, password, avatar, background } = body;
 
-// Serviço para obter todos os usuários
-const findAllService = () => User.find();
+  if (!name || !username || !email || !password || !avatar || !background)
+    throw new Error("Submit all fields for registration");
 
-// Serviço para obter um usuário pelo ID
-const findByIdService = (id) => User.findById(id);
+  const foundUser = await userRepository.findByEmailUser(data.email);
+  if (foundUser) throw new Error("User already exists");
 
-// Serviço para Atualizar um usuário
-const updateService = (
-  id,
-  name,
-  username,
-  email,
-  password,
-  avatar,
-  background
-) =>
-  User.findOneAndUpdate(
-    { _id: id },
-    { name, username, email, password, avatar, background }
+  const user = await userRepository.createRepository(body);
+
+  if (!user) throw new Error("Error Creating User");
+
+  const token = authService.generateToken(user.id);
+
+  return {
+    user: {
+      id: user._id,
+      name,
+      username,
+      email,
+      avatar,
+      background,
+    },
+    token,
+  };
+};
+
+// Função para obter todos os usuários
+const findAllService = async () => {
+  const users = await userRepository.findAllRepository();
+
+  if (users.length === 0) throw new Error("There are no registered users");
+
+  return users;
+};
+
+// Função para obter um usuário pelo ID
+const findByIdService = async (userId, userIdLogged) => {
+  let idParam;
+  if (!userId) {
+    userId = userIdLogged;
+    idParam = userId;
+  } else {
+    idParam = userId;
+  }
+  if (!idParam)
+    throw new Error("Send an ID in the parameters to search for the user");
+  const user = await userRepository.findByIdRepository(idParam);
+
+  return user;
+};
+
+// Função para atualizar um usuário
+const updateUserService = async (body, userId) => {
+  const { name, username, email, password, avatar, background } = body;
+  if (!name && !username && !email && !password && !avatar && !background)
+    throw new Error("Submit at least one field for update");
+
+  const user = await userRepository.findByIdRepository(id);
+
+  if (user._id != userId)
+    throw new Error(
+      "Submit at least one field for updateYou cannot update this user"
+    );
+
+  if (password) {
+    password = await bcrypt.hash(password, bcryptSaltRounds);
+  }
+
+  await userRepository.updateRepository(
+    userId,
+    body
   );
 
+  // Retorna a resposta de sucesso
+  return { message: "User successfully updated" };
+};
+
 export default {
+  createService,
   findAllService,
   findByIdService,
-  updateService,
-  createService,
+  updateUserService,
 };
