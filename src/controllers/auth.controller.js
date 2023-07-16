9b919fa5e0c6b5cb7a1477f33c70b9118c25d642
Walter.Netto <walter.netto@live.com>
2023-07-15 23:06:53 -0300

    refactor: turn token into object in response

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -2,10 +2,10 @@ import { loginService } from "../services/auth.service.js";
 
 export const login = async (req, res) => {
   const { email, password } = req.body;
-  
+
   try {
-    const token = await loginService( email, password );
-    return res.send(token);
+    const token = await loginService(email, password);
+    return res.send({ token });
   } catch (error) {
     console.error("Login error:", error);
     res.status(500).send({ message: "Login error" });
